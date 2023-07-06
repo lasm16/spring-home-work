1da46deb2251b6a075abba2c2794f058015eb518
19658272 <ASerLyubimov@sberbank.ru>
2023-07-06 21:39:16 +0300

    Create project

---
+++ src/test/java/com/springdemo/springhomework/SpringHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.springhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
